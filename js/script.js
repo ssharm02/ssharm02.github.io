c4d793092c64997fd0994453b94ba7c4b5d0a254
ssharm02 <sarthak.sharma02@gmail.com>
2018-03-31 10:19:21 -0400

    Fixed jquery, html and added stuff

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 var array = [];
 var projectData = [{
   title: 'Knights Tour C#',
-  text: '<p>C# Assignment for C# class, <a href="https://github.com/ssharm02/Knights_Tour_Algorithm" target="_blank"><i>Knights Tour</i></a><p>Made using C# features huristic & non huristic methods and Windows Form GUI</p>',
+  text: '<p>Assignment completed for C# during Summer 2017.  Implements "intelligent" heuristic method and non-intelligent method to traverse the chess board<br><b>URL: </b> <a href="https://github.com/ssharm02/Knights_Tour_Algorithm" target="_blank"><i>Knights Tour GitHub</i></a><p><b>Code:</b> C#<br><b>GUI: </b> Windows Forms</p>',
 
   image: ['../image/knightstour.png'],
   site: 
@@ -12,7 +12,7 @@ var projectData = [{
   ]
 }, {
   title: 'MasterMind Java',
-  text: '<p>Java 2 Assignment, <a href="https://github.com/ssharm02/Master-Mind" target="_blank"><i>MasterMind</i></a><br><p>Numerical MasterMind game<br>Made using Java</p>',
+  text: '<p>OOP Java 2 Assignment, numerical version of Mastermind <br><b>URL: </b><a href="https://github.com/ssharm02/Master-Mind" target="_blank"><i>MasterMind Java</i></a><br><p><b>Code:</b> Java<br><b>GUI:</b> Java Scene Builder</p>',
 
   image: ['../image/master.jpg'],
   site: 
@@ -21,7 +21,7 @@ var projectData = [{
   ]
 }, {
   title: 'Crypto Ticker iOS',
-  text: '<p>Swift project, <a href="https://github.com/ssharm02/Crypto_Coin_Ticker_iOS" target="_blank"><i>Crypto Currency Converter</i></a><br><p>Connects to crypto API, get real time prices and currency conversion<br>Made using Swift</p>',
+  text: '<p>Swift project.  Connects to a bitcoin API, get realtime currency conversion for various crypto coins. <br><b>URL: </b> <a href="https://github.com/ssharm02/Crypto_Coin_Ticker_iOS" target="_blank"><i>Crypto Currency Converter</i></a><br><p><b>Code:</b> Swift<br><b>Data:</b> BitCoinAverage API</p>',
 
   image: ['../image/crypto.jpg'],
   site: [
@@ -29,7 +29,7 @@ var projectData = [{
   ]
 }, {
   title: 'Weather App jQuery',
-  text: '<p>A Mobile App Development project, <a href="https://github.com/ssharm02/Mobile_Weather_App" target="_blank"><i>Mobile Weather App</i></a><br><p>Made for mobile devices<b>Uses jQuery, HTML5, CSS and Charts.js</p>',
+  text: '<p>A Mobile App Development, Get local weather via geolocation, visualize data using charts, search for weather <br> <b>URL: </b> <a href="https://github.com/ssharm02/Mobile_Weather_App" target="_blank"><i>Mobile Weather App</i></a><br><b>Code:</b> jQuery, HTML5, CSS, JavaScript<br><b>Data:</b> Open Weather Map API</p>',
 
   image: ['../image/weather.png'],
   site: [
@@ -37,7 +37,7 @@ var projectData = [{
   ]
 }, {
   title: 'Free Code Camp',
-  text: '<p>FreeCodeCamp Certification Work, <a href="https://github.com/ssharm02/Free_Code_Camp_Work" target="_blank"><i>FreeCodeCamp Repository</i></a><br><p>Basic, Intermediate, Advance Algorithms and Projects<br>HTML5, CSS, jQuery, Javascript</p>',
+  text: '<p>FreeCodeCamp Certification Work I have completed.  Includes Basic, Intermediate and Advance JavaScript algorithm functions and projects, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Free_Code_Camp_Work" target="_blank"><i>FreeCodeCamp Repository</i></a><br></p>',
 
   image: ['../image/freecc2.png'],
   site: [
@@ -47,38 +47,38 @@ var projectData = [{
 
 {
   title: 'WhatsApp Clone iOS',
-  text: '<p>Project completed for a Udemy Course, <a href="https://github.com/ssharm02/Chat_App_iOS" target="_blank"><i>whatsApp Clone for iOS devices</i></a><br><p>Uses Google Firebase for authentication and storing messages<br>Made Using Swift</p>',
+  text: '<p>Project completed for a Udemy Course, store user text messages on Google Firebase. User registeration & authentication also handeled via Firebase, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Chat_App_iOS" target="_blank"><i>WhatsApp Clone</i></a><br><p><b>Code:</b> Swift<br><b>Database:</b> Google Firebase</p>',
 
   image: ['../image/chat.jpg'],
   site: [
-    ['CodePen', 'fa-github', 'https://github.com/ssharm02/Chat_App_iOS']
+    ['Github', 'fa-github', 'https://github.com/ssharm02/Chat_App_iOS']
   ]
 },
 {
   title: 'Make Change Java',
-  text: '<p>Java 2 Assignment <a href="https://github.com/ssharm02/Make_Change_GUI" target="_blank"><i></i></a><br><p>Made using Java and Java Scene Builder</p>',
+  text: '<p>OOP Java 2 Assignment Shows user bill coins to dispense when change is entered., <br><b>URL: </b><a href="https://github.com/ssharm02/Make_Change_GUI" target="_blank"><i>Make Change GitHub link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java FX</p>',
 
   image: ['../image/change.jpg'],
   site: [
-    ['CodePen', 'fa-github', 'https://github.com/ssharm02/Make_Change_GUI']
+    ['Github', 'fa-github', 'https://github.com/ssharm02/Make_Change_GUI']
   ]
 },
 {
   title: 'Encryption Java',
-  text: '<p>Java 2 Assignment <a href="https://github.com/ssharm02/Hash_Map_Encryption" target="_blank"><i></i></a><br><p>Made using Java hash maps and Java Scene Builder</p>',
+  text: '<p>Java 2 Assignment.  Implements file reading and writing, encypt text from files and text-field, <br> <b>URL: </b><a href="https://github.com/ssharm02/Hash_Map_Encryption" target="_blank"><i>Encryption Java Link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java Scene Builder</p>',
 
   image: ['../image/encrypt.jpg'],
   site: [
-    ['CodePen', 'fa-github', 'https://github.com/ssharm02/Hash_Map_Encryption']
+    ['Github', 'fa-github', 'https://github.com/ssharm02/Hash_Map_Encryption']
   ]
 },
 {
   title: 'Clima iOS',
-  text: '<p>Clima iOS <a href="https://github.com/ssharm02/Clima_iOS" target="_blank"><i></i></a><br><p>Made using Swift, Cocoapods, LocationManager, Weather API</p>',
+  text: '<p>Clima iOS, get location via location manager and show local weather, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Clima_iOS" target="_blank"><i>Clima iOS Github</i></a><br><p><b>Code:</b> Swift<br><b>Data:</b> Open Weather Map API</p>',
 
   image: ['../image/clima.jpg'],
   site: [
-    ['CodePen', 'fa-github', 'https://github.com/ssharm02/Clima_iOS']
+    ['Github', 'fa-github', 'https://github.com/ssharm02/Clima_iOS']
   ]
 }
 ];
@@ -92,7 +92,10 @@ var projectData = [{
     }
 
     function clickListener(card) {
-      card.addEventListener("click", function() {
+      card.addEventListener("mouseover", function() {
+        this.classList.toggle("flipped");
+      });
+      card.addEventListener("mouseout", function() {
         this.classList.toggle("flipped");
       });
     }
@@ -110,7 +113,8 @@ var projectData = [{
   }
 }
 
-  function showProjectCards() {
+
+function showProjectCards() {
     var html = '';
 
     console.log(projectData.length);
@@ -129,10 +133,12 @@ var projectData = [{
       html += '<div class="project-title">' + project.title + '</div>';
       html += '<figcaption">';
       html += '<div class="project-body">' + project.text + '</div>';
-      html += '<div><a data-toggle="tooltip" title="' + project.site[0][0] + '" data-placement="top" href="' + project.site[0][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk0"><i class="fa ' + project.site[0][1] + '" aria-hidden="true"></i></a>';
+      html += '<div><a data-toggle="tooltip" title="' + project.site[0][0] + '" data-placement="top" href="' + project.site[0][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk0"><i class="fa ' 
+      + project.site[0][1] + '" aria-hidden="true"></i></a>';
 
       if (typeof project.site[1] !== 'undefined') {
-        html += '<a data-toggle="tooltip" title="' + project.site[1][0] + '" data-placement="top" href="' + project.site[1][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk1"><i class="fa ' + project.site[1][1] + '" aria-hidden="true"></i></a>';
+        html += '<a data-toggle="tooltip" title="' + project.site[1][0] + '" data-placement="top" href="' + project.site[1][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk1"><i class="fa '
+         + project.site[1][1] + '" aria-hidden="true"></i></a>';
       }
 
       html += '</div></figcaption></figure></div>';
@@ -146,11 +152,13 @@ var projectData = [{
   }
   showProjectCards();
 
+
   $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
      }, 500);
-});
+    });
+  
 });
