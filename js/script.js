38ad1628bc866d6dc6da41aff888b15e839b2c17
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2018-09-15 15:29:05 -0400

    minor edit

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
     ]
   },
   {
-    title: 'FlappyBird Clone Android',
+    title: 'FlappyBird Android',
     text: '<p>Android 2D-Game. <br><b>URL: </b> <a href="https://github.com/ssharm02/FlappyBird-Android_GDX" target="_blank"><i>FlappyBird Android Github</i></a><br><p><b>Code:</b> Java, Android Studio, GDX Library Android<br><b></p>',
 
     image: ['../image/flappybird.png'],
