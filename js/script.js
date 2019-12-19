346a7c523c8d030625dd368f95cc3a50e760bfe5
Sarthak <sarthak.sharma02@gmail.com>
2019-12-19 18:56:55 -0500

    updated some stuff

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //Project cards
-  let projectData = [
-    {
+  const projectData = [{
       title: 'Knights Tour C#',
       Developed: 'Developed in Summer 2017 for C# class assignment',
-      text:
-        '<p>Assignment completed for C# during Summer 2017.  Implements heuristic method and non-intelligent method<br><b>URL: </b> <a href="https://github.com/ssharm02/Knights_Tour_Algorithm" target="_blank"><i>Knights Tour GitHub</i></a><p><b>Code:</b> C#<br><b>GUI: </b> Windows Forms</p>',
+      text: '<p>Assignment completed for C# during Summer 2017.  Implements heuristic method and non-intelligent method<br><b>URL: </b> <a href="https://github.com/ssharm02/Knights_Tour_Algorithm" target="_blank"><i>Knights Tour GitHub</i></a><p><b>Code:</b> C#<br><b>GUI: </b> Windows Forms</p>',
 
       image: ['../image/knightstour.png'],
       site: [
@@ -19,8 +17,7 @@ $(document).ready(function() {
     {
       title: 'edu-counselor MERN',
       Developed: 'Sep 2018 - Dec 2018 for Sheridan Capstone Project',
-      text:
-        '<p>Full stack MEARN application made using latest Javascript technologies. <br><b>URL: </b> <a href="https://cryptic-ocean-62097.herokuapp.com/" target="_blank"><i>React Web App</i></a><br><p><b>Code:</b> JavaScript<br><b>Stack:</b> mongoDB, Redux, React, nodeJS, JavaScript</p>',
+      text: '<p>Full stack MEARN application made using latest Javascript technologies. <br><b>URL: </b> <a href="https://cryptic-ocean-62097.herokuapp.com/" target="_blank"><i>React Web App</i></a><br><p><b>Code:</b> JavaScript<br><b>Stack:</b> mongoDB, Redux, React, nodeJS, JavaScript</p>',
 
       image: ['../image/EduCoun.PNG'],
       site: [
@@ -34,8 +31,7 @@ $(document).ready(function() {
     {
       title: 'Weather App jQuery',
       Developed: 'Summer 2017 for jQuery class',
-      text:
-        '<p>Mobile App Development Final Project, Gets local weather via geolocation, visualize data using charts.js <br><b>URL: </b> <a href="https://github.com/ssharm02/Mobile_Weather_App" target="_blank"><i>Mobile Weather App</i></a><br><b>Code:</b> jQuery, HTML5, CSS, JavaScript<br><b>Data:</b> Open Weather Map API</p>',
+      text: '<p>Mobile App Development Final Project, Gets local weather via geolocation, visualize data using charts.js <br><b>URL: </b> <a href="https://github.com/ssharm02/Mobile_Weather_App" target="_blank"><i>Mobile Weather App</i></a><br><b>Code:</b> jQuery, HTML5, CSS, JavaScript<br><b>Data:</b> Open Weather Map API</p>',
 
       image: ['../image/weather.png'],
       site: [
@@ -49,8 +45,7 @@ $(document).ready(function() {
     {
       title: 'Free Code Camp',
       Developed: 'Started 2017 - ongoing part of learning',
-      text:
-        '<p>FreeCodeCamp JavaScript Work.  Contains lots of JavaScript learning material I completed, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Free_Code_Camp_Work" target="_blank"><i>FreeCodeCamp Repository</i></a><br></p>',
+      text: '<p>FreeCodeCamp JavaScript Work.  Contains lots of JavaScript learning material I completed, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Free_Code_Camp_Work" target="_blank"><i>FreeCodeCamp Repository</i></a><br></p>',
 
       image: ['../image/JavaScript.png'],
       site: [
@@ -64,8 +59,7 @@ $(document).ready(function() {
     {
       title: 'WhatsApp Clone iOS',
       Developed: 'January 2018 for iOS class @ Sheridan',
-      text:
-        '<p>Stores user text messages on Google Firebase. User registeration & authentication also handeled via Firebase, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Chat_App_iOS" target="_blank"><i>WhatsApp Clone</i></a><br><p><b>Code:</b> Swift<br><b>Database:</b> Google Firebase</p>',
+      text: '<p>Stores user text messages on Google Firebase. User registeration & authentication also handeled via Firebase, <br> <b>URL: </b> <a href="https://github.com/ssharm02/Chat_App_iOS" target="_blank"><i>WhatsApp Clone</i></a><br><p><b>Code:</b> Swift<br><b>Database:</b> Google Firebase</p>',
 
       image: ['../image/chat.jpg'],
       site: [
@@ -75,8 +69,7 @@ $(document).ready(function() {
     {
       title: 'Encryption Java',
       Developed: 'April 2017 for OOP Java 2',
-      text:
-        '<p>OOP Java 2 Assignment.  Implements file reading and writing, encypt text from files and text-field, <br> <b>URL: </b><a href="https://github.com/ssharm02/Hash_Map_Encryption" target="_blank"><i>Encryption Java Link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java Scene Builder</p>',
+      text: '<p>OOP Java 2 Assignment.  Implements file reading and writing, encypt text from files and text-field, <br> <b>URL: </b><a href="https://github.com/ssharm02/Hash_Map_Encryption" target="_blank"><i>Encryption Java Link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java Scene Builder</p>',
 
       image: ['../image/encrypt.jpg'],
       site: [
@@ -89,8 +82,7 @@ $(document).ready(function() {
     },
     {
       title: 'NutriFit iOS',
-      text:
-        '<p>Abandoned Capstone Project @ Sheridan. <br><b>URL: </b> <a href="https://github.com/ssharm02/Nutrition_App_iOS" target="_blank"><i>NutriFit iOS Github</i></a><br><p><b>Code:</b> Swift<br><b>Halted!</p>',
+      text: '<p>Abandoned Capstone Project @ Sheridan. <br><b>URL: </b> <a href="https://github.com/ssharm02/Nutrition_App_iOS" target="_blank"><i>NutriFit iOS Github</i></a><br><p><b>Code:</b> Swift<br><b>Halted!</p>',
 
       image: ['../image/Nutrition.jpg'],
       site: [
@@ -103,8 +95,7 @@ $(document).ready(function() {
     },
     {
       title: 'Helicopter Wars',
-      text:
-        '<p>Android 2D-Game. Random stages, music, collision detection <br><b>URL: </b> <a href="https://github.com/ssharm02/HelicopterWars-Libgdx" target="_blank"><i>Helicopter Wars Android Github</i></a><br><p><b>Code:</b> Java, Android Studio, GDX Library Android<br><b></p>',
+      text: '<p>Android 2D-Game. Random stages, music, collision detection <br><b>URL: </b> <a href="https://github.com/ssharm02/HelicopterWars-Libgdx" target="_blank"><i>Helicopter Wars Android Github</i></a><br><p><b>Code:</b> Java, Android Studio, GDX Library Android<br><b></p>',
 
       image: ['../image/HeliOne.png'],
       site: [
@@ -117,8 +108,7 @@ $(document).ready(function() {
     },
     {
       title: 'Space Dodger iOS',
-      text:
-        '<p>Spritkit Game. high scores dBase, tvOS, mapkit <br><b>URL: </b> <a href="https://github.com/ssharm02/SpaceDodger" target="_blank"><i>Space Dodger Swift Spritekit Github</i></a><br><p><b>Code:</b> Swift, Xcode, SpriteKit<br><b></p>',
+      text: '<p>Spritkit Game. high scores dBase, tvOS, mapkit <br><b>URL: </b> <a href="https://github.com/ssharm02/SpaceDodger" target="_blank"><i>Space Dodger Swift Spritekit Github</i></a><br><p><b>Code:</b> Swift, Xcode, SpriteKit<br><b></p>',
 
       image: ['../image/UFO.png'],
       site: [
@@ -127,8 +117,7 @@ $(document).ready(function() {
     },
     {
       title: 'Make Change Java',
-      text:
-        '<p>OOP Java 2 Assignment. <br> Shows user bill coins to dispense when change is entered., <br><b>URL: </b><a href="https://github.com/ssharm02/Make_Change_GUI" target="_blank"><i>Make Change GitHub link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java FX</p>',
+      text: '<p>OOP Java 2 Assignment. <br> Shows user bill coins to dispense when change is entered., <br><b>URL: </b><a href="https://github.com/ssharm02/Make_Change_GUI" target="_blank"><i>Make Change GitHub link</i></a><br><b>Code:</b> Java <br><b>GUI: </b>Java FX</p>',
 
       image: ['../image/change.jpg'],
       site: [
@@ -137,16 +126,16 @@ $(document).ready(function() {
     },
     {
       title: 'MasterMind Java',
-      text:
-        '<p>OOP Java 2 Assignment. <br> Numerical version of popular Mastermind game <br><b>URL: </b><a href="https://github.com/ssharm02/Master-Mind" target="_blank"><i>MasterMind Java</i></a><br><p><b>Code:</b> Java<br><b>GUI:</b> System.out.println :P</p>',
+      text: '<p>OOP Java 2 Assignment. <br> Numerical version of popular Mastermind game <br><b>URL: </b><a href="https://github.com/ssharm02/Master-Mind" target="_blank"><i>MasterMind Java</i></a><br><p><b>Code:</b> Java<br><b>GUI:</b> System.out.println :P</p>',
 
       image: ['../image/mastermind_logo_new.jpg'],
-      site: [['Github', 'fa-github', 'https://github.com/ssharm02/Master-Mind']]
+      site: [
+        ['Github', 'fa-github', 'https://github.com/ssharm02/Master-Mind']
+      ]
     },
     {
       title: 'Jeopardy Angular',
-      text:
-        '<p>Angular project.  Connects to opentdb API, get random questions. <br><b>URL: </b> <a href="https://github.com/ssharm02/JeopardyAngular" target="_blank"><i>Angular Jeopardy Game</i></a><br><p><b>Code:</b> Angular<br></p>',
+      text: '<p>Angular project.  Connects to opentdb API, get random questions. <br><b>URL: </b> <a href="https://github.com/ssharm02/JeopardyAngular" target="_blank"><i>Angular Jeopardy Game</i></a><br><p><b>Code:</b> Angular<br></p>',
 
       image: ['../image/Jeopardy.jpg'],
       site: [
@@ -164,10 +153,10 @@ $(document).ready(function() {
     }
 
     function clickListener(card) {
-      card.addEventListener('mouseover', function() {
+      card.addEventListener('mouseover', function () {
         this.classList.toggle('flipped');
       });
-      card.addEventListener('mouseout', function() {
+      card.addEventListener('mouseout', function () {
         this.classList.toggle('flipped');
       });
     }
@@ -191,44 +180,28 @@ $(document).ready(function() {
     //randomize the projects
     randomizeProjects();
 
-    projectData.forEach(function(project) {
-      html += '<div class="col-sm-6 col-md-4">';
-      html += '<div class="card effect_click"><div class="card_front">';
-      html +=
-        '<figure><img class="img-responsive" src="img/' +
-        project.image[0] +
-        '">';
-      html += '<figcaption class="project-title">';
-      html += project.title;
+    projectData.forEach(function (project) {
+      html += `<div class="col-sm-6 col-md-4">`;
+      html += `<div class="card effect_click"><div class="card_front">`;
+      html += `<figure><img class="img-responsive" src="img/${project.image[0]}">`;
+      html += `<figcaption class="project-title">`;
+      html += `${project.title}`;
       html += '</figcaption></figure></div>';
 
-      html += '<div class="card_back"><figure>';
-      html += '<div class="project-title">' + project.title + '</div>';
-      html += '<figcaption">';
-      html += '<div class="project-body">' + project.text + '</div>';
-      html +=
-        '<div><a data-toggle="tooltip" title="' +
-        project.site[0][0] +
-        '" data-placement="top" href="' +
-        project.site[0][2] +
-        '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk0"><i class="fa ' +
-        project.site[0][1] +
-        '" aria-hidden="true"></i></a>';
+      html += `<div class="card_back"><figure>`;
+      html += `<div class="project-title">${project.title}</div>`;
+      html += `<figcaption">`;
+      html += `<div class="project-body">${project.text}</div>`;
+      html += `<div><a data-toggle="tooltip" title="${project.site[0][0]}" data-placement="top" href="${project.site[0][2]}" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk0"><i class="fa ${project.site[0][1]}" aria-hidden="true"></i></a>`;
 
       if (typeof project.site[1] !== 'undefined') {
         html +=
-          '<a data-toggle="tooltip" title="' +
-          project.site[1][0] +
-          '" data-placement="top" href="' +
-          project.site[1][2] +
-          '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk1"><i class="fa ' +
-          project.site[1][1] +
-          '" aria-hidden="true"></i></a>';
+          `<a data-toggle="tooltip" title="${project.site[1][0]}" data-placement="top" href="${project.site[1][2]}" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk1"><i class="fa${project.site[1][1]}" aria-hidden="true"></i></a>`;
       }
 
-      html += '</div></figcaption></figure></div>';
-      html += '</div>';
-      html += '</div>';
+      html += `</div></figcaption></figure></div>`;
+      html += `</div>`;
+      html += `</div>`;
     });
 
     $('#theProjects').append(html);
